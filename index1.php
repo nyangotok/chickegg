df083de72d674369917e90d7e38eac5b625edc8d
nyangotok <104475538+nyangotok@users.noreply.github.com>
2022-09-28 08:25:49 +0300

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,9 @@
+<?php
+include('include/connect.php');
+include('include/header.php');
+include('include/clerkDashboard.php');
+include('include/clerkMenu.php');
+include('include/scripts.php');
+include('include/footer.php');
+?>
+ 
---
+++ log.php
@@ -0,0 +1,41 @@
+<?php
+       include('include/connect.php');
+       include('include/header.php');
+       include('include/sidebar1.php')
+       
+        ?>
+<div id="content-wrapper">
+
+        <div class="container-fluid">
+ <h2><u>Sytstem Access Log History</u></h2> 
+            <div class="card-body">
+              <div class="table-responsive">
+                <table class="table table-bordered" id="" width="100%" cellspacing="0">
+                  <thead>
+                    <tr>
+                      <th>Logs</th>
+                    </tr>
+                  </thead>
+                  
+              <?php
+$query = "SELECT * FROM logs";
+                    $result = mysqli_query($db, $query) or die (mysqli_error($db));
+                  
+                        while ($row = mysqli_fetch_assoc($result)) {
+                                             
+                            echo '<tr>';
+                             echo '<td>'. $row['action'].'---'.$row['date_time'].'</td>';
+                            echo '</tr> ';
+
+                               
+                        }
+                        
+            ?> 
+           
+                </table>
+                <?php
+
+              include('include/scripts.php');
+       include('include/footer.php');
+       
+        ?>
